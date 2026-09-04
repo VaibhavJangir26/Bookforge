e5e0ea3b4d6e61b1924fb132e992d039dd75742a
Vaibhav Jangir <idforgames0000@gmail.com>
2026-09-04 16:52:10 +0530

    fix db issue and correct fix eureka issue

--- auth-service/src/main/java/com/bluewave/config/SecurityConfig.java
+++ auth-service/src/main/java/com/bluewave/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/eureka/**", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2->oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()).jwtAuthenticationConverter(jwtAuthenticationConverter())))
